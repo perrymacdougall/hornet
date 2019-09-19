90be276a5eca2b334d9c5b3775ec9209c1c14b97
perrymacdougall <perrymacdougall@gmail.com>
2019-09-19 05:13:14 -0400

    configured nav menu options

--- functions.php
+++ functions.php
@@ -1,4 +1,19 @@
 <?php
+
+if ( ! function_exists( 'hornet_setup' ) ) :
+    function hornet_setup() {
+        register_nav_menus(
+            array(
+                'Primary' => __( 'Header Menu', 'hornet' ),
+                'Footer' => __( 'Footer Menu', 'hornet' ),
+                'Social' => __( 'Social Links', 'hornet' )
+            )
+        );
+    }
+endif;
+
+add_action( 'after_setup_theme', 'hornet_setup' );
+
 function add_theme_scripts() {
     wp_enqueue_style( 'style', get_stylesheet_uri() );
 }
